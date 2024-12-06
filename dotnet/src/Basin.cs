8762e6ec7eca45944a206fb22cdca20e47b83cac
Justin D. Harris <juharris@users.noreply.github.com>
2024-12-06 18:11:31 -0500

    Start test

--- dotnet/src/Basin.cs
+++ dotnet/src/Basin.cs
@@ -159,10 +159,9 @@
 		/// <param name="path">A JSONPath.</param>
 		/// <returns>A JSON Pointer</returns>
 		/// <remarks>
-		/// Exposed for testing.
-		/// ASSUMPTION: Only handles simple cases. E.g., key names cannot have &quot;[&quot; or &quot;]&quot;.
+		/// ASSUMPTION: Only handles simple cases. E.g., key names within the path cannot have &quot;[&quot; or &quot;]&quot;.
 		/// </remarks>
-		public static string ConvertJsonPathToJsonPointer(string path)
+		internal static string ConvertJsonPathToJsonPointer(string path)
 		{
 			// TODO Optimize to only go through the path once.
 			var resultBuilder = new StringBuilder(path);
@@ -193,7 +192,7 @@
 			return resultBuilder.ToString();
 		}
 
-		private static string GetTopLevelKey(string pointer)
+		internal static string GetTopLevelKey(string pointer)
 		{
 			// Assume it starts with a "/" and remove it.
 			var endIndex = pointer.IndexOf('/', 1);
---
+++ dotnet/tests/Tests/MyClass.cs
@@ -0,0 +1,10 @@
+﻿namespace ObjectBasin.Tests;
+
+using System.Text.Json;
+
+internal class MyClass
+{
+	public string? Text { get; set; }
+
+	public List<JsonElement>? Elements { get; set; }
+}
